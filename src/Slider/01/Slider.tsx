a09ad816c1f195580c3f2f8a4cbaa149247552bd
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2023-05-03 11:01:52 +0900

    feature: implement infinity image slide, used transition animation

--- src/Slider/01/Slider.tsx
+++ src/Slider/01/Slider.tsx
@@ -1,36 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./slider.css";
 import SliderButton from "./SliderButton";
 
 const Slider = (): JSX.Element => {
   const slides: string[] = ["#33a", "#8c9", "#f3e074"];
+
+  const [slidesList, setSlidesList] = useState<string[]>([]);
   const [currentIndex, setCurrentIndex] = useState<number>(1);
+  const [currentTrans, setCurrentTrans] = useState<string>("transform 0.1s ease");
 
-  // const setSlides = () => {
-  //   let front = [];
-  //   let last = [];
-  //   let index = 0;
+  const resetSlides = (type: string) => {
+    const nextIndex = type === 'next' ? currentIndex + 1 : currentIndex - 1;
+    setCurrentIndex(nextIndex);
 
-  //   while (index < 2) {
-  //     front.push(slides[index % slides.length]);
-  //     last.unshift(slides[slides.length - 1 - (index % slides.length)]);
-  //     index++;
-  //   }
+    setTimeout(() => {
+      setCurrentTrans("none");
+      setCurrentIndex(Math.floor(slidesList.length / 2));
 
-  //   return [...front, ...slides, ...last];
-  // };
-
-  // let showSlides = setSlides();
+      setTimeout(() => {
+        setCurrentTrans("transform 0.1s ease");
+      }, 100);
+    }, 100);
+  }
 
   const onSliderBtn = (type: string) => {
-    if (type === "next" && currentIndex < slides.length - 1) {
-      setCurrentIndex(currentIndex + 1);
+    if (type === "next") {
+      if(currentIndex === slides.length + 3) {
+        // setCurrentIndex(currentIndex + 1);
+        resetSlides(type);
+      } else {
+        setCurrentIndex(currentIndex + 1);
+      }
     }
 
-    if (type === "prev" && currentIndex > 0) {
-      setCurrentIndex(currentIndex - 1);
+    if (type === "prev") {
+      if(currentIndex === slides.length - 1) {
+        resetSlides(type);
+      } else {
+        setCurrentIndex(currentIndex - 1);
+      }
     }
   };
+
+  useEffect(() => {
+    const first = [slides[0], slides[1], slides[2]];
+    const last = [slides[slides.length - 3], slides[slides.length - 2], slides[slides.length - 1]];
+    setSlidesList([...last, ...slides, ...first]);
+    setCurrentIndex(Math.floor([...last, ...slides, ...first].length / 2))
+    // eslint-disable-next-line
+  }, [])
 
   return (
     <div className="slider-area">
@@ -39,13 +57,13 @@ const Slider = (): JSX.Element => {
       <div
         className="slider-track"
         style={{
-          transform: `translateX(calc(${(-100 / slides.length) * (0.5 + currentIndex)}%)`,
-          transition: `transform 0.3s ease`,
+          transform: `translateX(calc(${(-100 / slidesList.length) * (0.5 + currentIndex)}%)`,
+          transition: `${currentTrans}`,
         }}
       >
-        {slides.map((color: string, index: number): JSX.Element => (
-          <div className={`slider-item ${currentIndex !== index ? "non" : ""}`} style={{background: color}}>
-            <div><span>{index}</span></div>
+        {slidesList.map((color: string, index: number): JSX.Element => (
+          <div key={index} className={`slider-item ${currentIndex !== index ? "non" : ""}`} style={{background: color}}>
+            {/* <div><span>{index}</span></div> */}
           </div>
         ))}
       </div>
